8703a8e71ba3a736247a27e79a77f82edff7af86
Hideaki IWASE <i.poper@gmail.com>
2024-06-07 22:22:57 +0900

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,122 @@
+# xlsx_extractor
+
+Tool to extract data from xlsx(xlsm) by specifying headers.
+
+## Description
+
+**xlsx_extractor** is extracts data by specifying a header string.
+
+For example, if you have the following Excel files and specify `test1` and `test2` as headers, you will get `ab,cd` and extraction results.
+
+![figure1.png](graph/figure1.png)
+
+```
+$ xlsx_extractor --file test.xlsx -d , test1 test2
+test1,test2
+ab,cd
+```
+
+Find the rows in the sheet where all the specified headers exist, and output the data after the headers in the order of the specified headers.
+
+```
+$ xlsx_extractor --file test.xlsx -s test_sheet -d , test2 test1 test3
+test2,test1,test3
+cd,ab,3
+b,a,c
+```
+
+## Usage
+```
+Tool to extract data from xlsx(xlsm) by specifying headers.
+
+Usage: xlsx_extractor [OPTIONS] --file <XLSX> [HEADERS]...
+
+Arguments:
+  [HEADERS]...  Header names
+
+Options:
+  -f, --file <XLSX>            Excel file(.xlsx or .xlsm)
+  -d, --delimiter <DELIMITER>  Output delimiter [default: "\t"]
+  -s, --sheet <SHEET>          Sheet name
+  -H, --header                 Suppress header output
+  -q, --quote <QUOTE>          Quote [default: "]
+  -t, --style <STYLE>          Quote Style [default: necessary] [possible values: always, necessary, non-numeric, never]
+  -o, --output <FILE>          Place the output into <FILE>
+  -h, --help                   Print help
+  -V, --version                Print version
+```
+
+The tool searches for cells with the header names specified by HEADERS, starting from the upper left corner of the sheet. The rows in which all headers are found are recognized as headers.
+
+![figure2.png](graph/figure2.png)
+
+```
+$ xlsx_extractor --file test.xlsx -s test_sheet -d , test2 test1 test3
+```
+When you execute the above command on a sheet like the one shown in the figure, the tool first looks for the header name starting from cell A1 in the right direction. If the header is not found in the row, it proceeds to the next row. In the figure, cell I3 contains the letters specified as headers, but not all the headers specified are present, so the tool does not recognize it as a header row.
+Since all the header strings are present in line 7, this line is recognized as the header line.
+The next line after the header line is the data line. In the figure, columns B, C, and D with arrows are extracted. The output order of the columns is the order specified in HEADERS.
+
+As a result,
+```
+test2,test1,test3
+cd,ab,3
+b,a,c
+```
+is output.
+
+### Escape sequence
+
+You can use escape sequences for headers.If you wish to designate cell E7 as a header, do the following
+```
+$ xlsx_extractor --file test.xlsx -s test_sheet -d , test2 test1 test3 Test\\r\\n4
+test2,test1,test3,"Test
+4"
+cd,ab,3,4
+b,a,c,c
+```
+Headers containing newlines are enclosed in double quotation marks. The default setting is to quote when necessary.
+
+You can use escape sequences also in delimiters and quotes.
+```
+$ xlsx_extractor --f test.xlsx -s test_sheet -d \\t -q \' test2 test1 Test\\r\\n4
+test2   test1   test3   'Test
+4'
+cd      ab      3       4
+b       a       c       c
+```
+
+### Quoting style
+You can specify a quoting style.
+- always
+  ```
+  $ xlsx_extractor --f test.xlsx -s test_sheet -d , -t always test2 test1 Test\\r\\n4
+  "test2","test1","test3","Test
+  4"
+  "cd","ab","3","4"
+  "b","a","c","c"
+  ```
+- necessary
+  ```
+  $ xlsx_extractor --f test.xlsx -s test_sheet -d , -t necessary test2 test1 Test\\r\\n4
+  test2,test1,test3,"Test
+  4"
+  cd,ab,3,4
+  b,a,c,c
+  ```
+- non-numeric
+  ```
+  $ xlsx_extractor --f test.xlsx -s test_sheet -d , -t non-numeric test2 test1 Test\\r\\n4
+  "test2","test1","test3","Test
+  4"
+  "cd","ab",3,4
+  "b","a","c","c"
+  ```
+- never
+  ```
+  $ xlsx_extractor --f test.xlsx -s test_sheet -d , -t never test2 test1 Test\\r\\n4
+  test2,test1,test3,Test
+  4
+  cd,ab,3,4
+  b,a,c,c
+  ```
---
+++ src/main.rs
@@ -0,0 +1,190 @@
+use clap::{error::ErrorKind, ArgAction, CommandFactory, Parser};
+use clap_derive::{Parser, ValueEnum};
+use csv::{QuoteStyle, Writer, WriterBuilder};
+use std::{error::Error, io};
+use umya_spreadsheet::*;
+use unescape::unescape;
+
+#[derive(Parser, Debug)]
+#[command(version, about="Tool to extract data from xlsx(xlsm) by specifying headers.", long_about = None)]
+struct Args {
+    /// Excel file(.xlsx or .xlsm)
+    #[arg(short = 'f', long = "file")]
+    xlsx: String,
+    /// Output delimiter
+    #[arg(short, long, value_parser = escaped_u8, default_value="\t")]
+    delimiter: u8,
+    /// Sheet name
+    #[arg(short, long)]
+    sheet: Option<String>,
+    /// Suppress header output
+    #[arg(short='H', long, action=ArgAction::SetFalse, default_value_t=true)]
+    header: bool,
+    /// Quote
+    #[arg(short, long, value_parser = escaped_u8, default_value="\"")]
+    quote: u8,
+    /// Quote Style
+    #[arg(short='t', long, value_enum, default_value_t=Style::Necessary)]
+    style: Style,
+    /// Place the output into <FILE>
+    #[arg(short = 'o', long = "output")]
+    file: Option<String>,
+    /// Header names
+    #[arg(value_parser = escaped_string)]
+    headers: Vec<String>,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Style {
+    Always,
+    Necessary,
+    NonNumeric,
+    Never,
+}
+
+impl From<Style> for QuoteStyle {
+    fn from(value: Style) -> Self {
+        match value {
+            Style::Always => QuoteStyle::Always,
+            Style::Necessary => QuoteStyle::Necessary,
+            Style::NonNumeric => QuoteStyle::NonNumeric,
+            Style::Never => QuoteStyle::Never,
+        }
+    }
+}
+
+/// header information
+struct HeaderInfo {
+    /// Header row
+    row: u32,
+    /// Header column positions
+    header_column: Vec<u32>,
+}
+
+//--------------------------------------------------------------------------------
+/// Parsing of escape sequence char
+fn escaped_u8(s: &str) -> Result<u8, String> {
+    let d = unescape(s).ok_or(format!("`{s}` is not a valid escape string."))?;
+    let d = d.as_bytes();
+    if d.len() != 1 {
+        return Err("Specified by ASCII characters.".to_string());
+    }
+    Ok(d[0])
+}
+
+//--------------------------------------------------------------------------------
+/// Parsing of escape sequence strings
+fn escaped_string(s: &str) -> Result<String, String> {
+    unescape(s).ok_or(format!("`{s}` is not a valid escape string."))
+}
+
+//================================================================================
+/// main
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let path = std::path::Path::new(&args.xlsx);
+    let mut book = reader::xlsx::lazy_read(path).unwrap();
+
+    // Check sheet name
+    let sheet = if let Some(ref sheet_name) = args.sheet {
+        let index = book
+            .get_sheet_collection_no_check()
+            .iter()
+            .position(|x| x.get_name() == sheet_name)
+            .unwrap_or_else(|| invalid_value(&format!("Sheet not found:{}", sheet_name)));
+        book.read_sheet(index).get_sheet(&index).unwrap()
+    } else {
+        book.read_sheet(0_usize)
+            .get_sheet(&(0_usize))
+            .unwrap_or_else(|| invalid_value("There is no sheet."))
+    };
+
+    let mut binding = WriterBuilder::new();
+    let builder = binding
+        .delimiter(args.delimiter)
+        .quote(args.quote)
+        .quote_style(args.style.into());
+
+    // Find a Header
+    let header = find_header(sheet, &args.headers).unwrap_or_else(|| {
+        invalid_value("`[HEADERS]...` not found.");
+    });
+
+    let mut data_iter = (header.row + 1..=sheet.get_highest_row())
+        .map(|row| {
+            header
+                .header_column
+                .iter()
+                .map(|x| sheet.get_formatted_value((*x, row)))
+                .collect::<Vec<String>>()
+        })
+        .filter(|x| x.iter().all(|y| y.is_empty()));
+    let show_headers = args.header.then_some(args.headers);
+    // Output data based on headers
+    if let Some(output) = args.file {
+        let mut writer = builder
+            .from_path(&output)
+            .unwrap_or_else(|x| invalid_value(&format!("Can't create {output}: {x}")));
+        output_table_data(show_headers, &mut data_iter, &mut writer)
+    } else {
+        let mut writer = builder.from_writer(io::stdout());
+        output_table_data(show_headers, &mut data_iter, &mut writer)
+    }
+}
+
+//--------------------------------------------------------------------------------
+/// Output the data of the columns recognized as headers.
+fn output_table_data<W: io::Write>(
+    show_header: Option<Vec<String>>,
+    data_iter: &mut impl Iterator<Item = Vec<String>>,
+    writer: &mut Writer<W>,
+) -> Result<(), Box<dyn Error>> {
+    // Output headers
+    if let Some(header) = show_header {
+        writer.write_record(header)?;
+    }
+    // Output Datas
+    data_iter.try_for_each(|x| writer.write_record(x))?;
+    Ok(writer.flush()?)
+}
+
+//--------------------------------------------------------------------------------
+/// Find the rows in the sheet that contain all the characters specified as headers.
+fn find_header(sheet: &Worksheet, headers: &[String]) -> Option<HeaderInfo> {
+    for row in 1..=sheet.get_highest_row() {
+        if let Some(header_column) = find_header_in_row(row, sheet, headers) {
+            return Some(HeaderInfo { row, header_column });
+        }
+    }
+    None
+}
+
+//--------------------------------------------------------------------------------
+/// Determines whether a header exists in a row.
+fn find_header_in_row(row: u32, sheet: &Worksheet, headers: &[String]) -> Option<Vec<u32>> {
+    let mut indexes: Vec<(&String, Option<u32>)> =
+        headers.iter().map(|x| (x, None as Option<u32>)).collect();
+
+    for col in 1..=sheet.get_highest_column() {
+        let text = sheet.get_formatted_value((col, row));
+        if text.is_empty() || !headers.iter().any(|x| x == &text) {
+            continue;
+        }
+
+        if let Some(h) = indexes.iter_mut().find(|(h, c)| *h == &text && c.is_none()) {
+            h.1 = Some(col);
+        }
+        if indexes.iter().all(|(_, x)| x.is_some()) {
+            return Some(indexes.into_iter().map(|(_, c)| c.unwrap()).collect());
+        }
+    }
+    None
+}
+
+//--------------------------------------------------------------------------------
+/// Termination process when an illegal value is detected
+fn invalid_value(msg: &str) -> ! {
+    let mut cmd = Args::command();
+    cmd.error(ErrorKind::InvalidValue, msg).exit();
+}
